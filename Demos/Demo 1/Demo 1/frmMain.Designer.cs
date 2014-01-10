3f112158697473fcafe8e1ab7b141d3631b75523
AJ Armstrong <aja@nait.ca>
2014-01-10 08:49:24 -0700

    Initial Form Layout
    Added two controls, renamed them, no methods yet.

---
+++ Demos/Demo 1/Demo 1/frmMain.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Demo_1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMain = new System.Windows.Forms.Button();
+            this.lblText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMain
+            // 
+            this.btnMain.Location = new System.Drawing.Point(65, 45);
+            this.btnMain.Name = "btnMain";
+            this.btnMain.Size = new System.Drawing.Size(75, 23);
+            this.btnMain.TabIndex = 0;
+            this.btnMain.Text = "Click Me!";
+            this.btnMain.UseVisualStyleBackColor = true;
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(98, 9);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(0, 13);
+            this.lblText.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(201, 80);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.btnMain);
+            this.Name = "frmMain";
+            this.Text = "Demo 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMain;
+        private System.Windows.Forms.Label lblText;
+    }
+}
+
