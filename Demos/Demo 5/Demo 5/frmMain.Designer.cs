8f3c3f3cbe02623623b34ae2fb9a3b423e3336c5
AJ Armstrong <aja@nait.ca>
2014-01-24 09:03:14 -0700

    Demo 5 - Silly survey of controls.

---
+++ Demos/Demo 5/Demo 5/frmMain.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Demo_5
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGoogle = new System.Windows.Forms.Button();
+            this.btnNAIT = new System.Windows.Forms.Button();
+            this.btnNews = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.wbMain = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteWithLoveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteWithHateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.noHelpForYouDudeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGoogle
+            // 
+            this.btnGoogle.Location = new System.Drawing.Point(12, 3);
+            this.btnGoogle.Name = "btnGoogle";
+            this.btnGoogle.Size = new System.Drawing.Size(75, 23);
+            this.btnGoogle.TabIndex = 1;
+            this.btnGoogle.Text = "Google";
+            this.btnGoogle.UseVisualStyleBackColor = true;
+            this.btnGoogle.Click += new System.EventHandler(this.btnGoogle_Click);
+            // 
+            // btnNAIT
+            // 
+            this.btnNAIT.Location = new System.Drawing.Point(12, 32);
+            this.btnNAIT.Name = "btnNAIT";
+            this.btnNAIT.Size = new System.Drawing.Size(75, 23);
+            this.btnNAIT.TabIndex = 2;
+            this.btnNAIT.Text = "NAIT";
+            this.btnNAIT.UseVisualStyleBackColor = true;
+            this.btnNAIT.Click += new System.EventHandler(this.btnNAIT_Click);
+            // 
+            // btnNews
+            // 
+            this.btnNews.Location = new System.Drawing.Point(12, 61);
+            this.btnNews.Name = "btnNews";
+            this.btnNews.Size = new System.Drawing.Size(75, 23);
+            this.btnNews.TabIndex = 3;
+            this.btnNews.Text = "News";
+            this.btnNews.UseVisualStyleBackColor = true;
+            this.btnNews.Click += new System.EventHandler(this.btnNews_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnFile);
+            this.splitContainer1.Panel1.Controls.Add(this.btnGoogle);
+            this.splitContainer1.Panel1.Controls.Add(this.btnNews);
+            this.splitContainer1.Panel1.Controls.Add(this.btnNAIT);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.wbMain);
+            this.splitContainer1.Panel2.Controls.Add(this.menuStrip1);
+            this.splitContainer1.Size = new System.Drawing.Size(560, 453);
+            this.splitContainer1.SplitterDistance = 96;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // wbMain
+            // 
+            this.wbMain.Location = new System.Drawing.Point(3, 45);
+            this.wbMain.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbMain.Name = "wbMain";
+            this.wbMain.Size = new System.Drawing.Size(445, 258);
+            this.wbMain.TabIndex = 0;
+            this.wbMain.Url = new System.Uri("http://www.google.ca", System.UriKind.Absolute);
+            this.wbMain.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.wbMain_DocumentCompleted);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(460, 24);
+            this.menuStrip1.Stretch = false;
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pasteToolStripMenuItem,
+            this.copyToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "E&dit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.noHelpForYouDudeToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.CheckOnClick = true;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Enabled = false;
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pasteWithLoveToolStripMenuItem,
+            this.pasteWithHateToolStripMenuItem});
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            // 
+            // pasteWithLoveToolStripMenuItem
+            // 
+            this.pasteWithLoveToolStripMenuItem.Name = "pasteWithLoveToolStripMenuItem";
+            this.pasteWithLoveToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.pasteWithLoveToolStripMenuItem.Text = "Paste with Love";
+            // 
+            // pasteWithHateToolStripMenuItem
+            // 
+            this.pasteWithHateToolStripMenuItem.Name = "pasteWithHateToolStripMenuItem";
+            this.pasteWithHateToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.pasteWithHateToolStripMenuItem.Text = "Paste with Hate";
+            // 
+            // noHelpForYouDudeToolStripMenuItem
+            // 
+            this.noHelpForYouDudeToolStripMenuItem.Name = "noHelpForYouDudeToolStripMenuItem";
+            this.noHelpForYouDudeToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.noHelpForYouDudeToolStripMenuItem.Text = "No help for you, dude.";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(12, 90);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFile.TabIndex = 4;
+            this.btnFile.Text = "File";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 453);
+            this.Controls.Add(this.splitContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Browse It";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGoogle;
+        private System.Windows.Forms.Button btnNAIT;
+        private System.Windows.Forms.Button btnNews;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.WebBrowser wbMain;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteWithLoveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteWithHateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem noHelpForYouDudeToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnFile;
+
+
+    }
+}
+
---
+++ Demos/Demo 5/Demo 5/frmMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Demo_5
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoogle_Click(object sender, EventArgs e)
+        {
+            wbMain.Url = new Uri("http://www.google.ca");
+        }
+
+        private void btnNAIT_Click(object sender, EventArgs e)
+        {
+            wbMain.Url = new Uri("http://www.nait.ca");
+        }
+
+        private void btnNews_Click(object sender, EventArgs e)
+        {
+            wbMain.Url = new Uri("http://news.ycombinator.com");
+        }
+
+        private void wbMain_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //MessageBox.Show(string.Format("You have navigated to: {0}", wbMain.Url));
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Save me, for god's sake.  There's room for two on that piece of wood!");
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You can check me!");
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            MessageBox.Show(saveFileDialog1.FileName);
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = 
+                Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            saveFileDialog1.ShowDialog();
+        }
+    }
+}
