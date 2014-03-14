6de3326a50d4bfa81b0f519d18fcdb00942b2ff9
AJ Armstrong <aja@nait.ca>
2014-03-14 08:27:46 -0600

    Added Dialog form

---
+++ Demos/Demo 7/Demo 7/frmDialog.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Demo_7
+{
+    partial class frmDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDismiss = new System.Windows.Forms.Button();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDismiss
+            // 
+            this.btnDismiss.Location = new System.Drawing.Point(127, 84);
+            this.btnDismiss.Name = "btnDismiss";
+            this.btnDismiss.Size = new System.Drawing.Size(75, 23);
+            this.btnDismiss.TabIndex = 0;
+            this.btnDismiss.Text = "Dismiss";
+            this.btnDismiss.UseVisualStyleBackColor = true;
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(4, 13);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(80, 13);
+            this.lblFrom.TabIndex = 9;
+            this.lblFrom.Text = "Text from Form:";
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(4, 46);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(69, 13);
+            this.lblTo.TabIndex = 8;
+            this.lblTo.Text = "Text to Form:";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(92, 12);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(233, 20);
+            this.txtFrom.TabIndex = 7;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(92, 43);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(233, 20);
+            this.txtTo.TabIndex = 6;
+            // 
+            // frmDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 123);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.btnDismiss);
+            this.Name = "frmDialog";
+            this.Text = "Dialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDismiss;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+    }
+}
