854bbafab5e53bfaaccad64b490bc2ca295c2ff7
AJ Armstrong <aja@nait.ca>
2015-01-12 12:10:29 -0700

    Final version.

---
+++ Winter 2015/Demo 1/Demo 1/frmMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Demo_1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.button = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(12, 54);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 0;
+            this.button.Text = "Click Me!";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            this.button.MouseEnter += new System.EventHandler(this.button_MouseEnter);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(12, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 13);
+            this.label.TabIndex = 1;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(155, 100);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Demo 1";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.frmMain_HelpButtonClicked);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseClick);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseDoubleClick);
+            this.MouseEnter += new System.EventHandler(this.frmMain_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.frmMain_MouseLeave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ Winter 2015/Demo 1/Demo 1/frmMain.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_1
+{
+    public partial class frmMain : Form
+    {
+        //We can do custom private member variables
+        private int clickCount = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Are you sure you want to close this application?", "Closing",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.No)
+                e.Cancel = true;
+        }
+
+        private void frmMain_MouseClick(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("That Tickles! (" + e.X + "," + e.Y + ")");
+        }
+
+        private void frmMain_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            MessageBox.Show("There's no help for someone like you.");
+        }
+
+        private void frmMain_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Stop it!  That hurts!");
+        }
+
+        private void frmMain_MouseEnter(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red; //"this" is the object.  In this case the form.
+            //Short for.
+        }
+
+        private void frmMain_MouseLeave(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            label.Text = incrementClickCount().ToString();
+        }
+
+        
+        //We can also creater our own private methods.
+        private int incrementClickCount()
+        {
+            return ++clickCount; //Increment then return the incremented value
+        }
+
+        
+        private void button_MouseEnter(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Yellow;
+        }
+
+
+    }
+}
