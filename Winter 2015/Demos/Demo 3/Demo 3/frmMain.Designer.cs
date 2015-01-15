58f0876f5d76a4f7c56d0f2300b4a55731d17782
AJ Armstrong <aja@nait.ca>
2015-01-15 09:59:47 -0700

    Demo 3 Added

---
+++ Winter 2015/Demos/Demo 3/Demo 3/frmMain.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Demo_3
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbrValue = new System.Windows.Forms.TrackBar();
+            this.tbxValue = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tbrValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbrValue
+            // 
+            this.tbrValue.Location = new System.Drawing.Point(12, 60);
+            this.tbrValue.Maximum = 110;
+            this.tbrValue.Minimum = 10;
+            this.tbrValue.Name = "tbrValue";
+            this.tbrValue.Size = new System.Drawing.Size(230, 45);
+            this.tbrValue.TabIndex = 0;
+            this.tbrValue.TickFrequency = 5;
+            this.tbrValue.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.tbrValue.Value = 55;
+            this.tbrValue.Scroll += new System.EventHandler(this.tbrValue_Scroll);
+            // 
+            // tbxValue
+            // 
+            this.tbxValue.Location = new System.Drawing.Point(70, 12);
+            this.tbxValue.Name = "tbxValue";
+            this.tbxValue.Size = new System.Drawing.Size(100, 20);
+            this.tbxValue.TabIndex = 1;
+            this.tbxValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbxValue.Leave += new System.EventHandler(this.tbxValue_Leave);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(254, 110);
+            this.Controls.Add(this.tbxValue);
+            this.Controls.Add(this.tbrValue);
+            this.Name = "frmMain";
+            this.Text = "Demo 3";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tbrValue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tbrValue;
+        private System.Windows.Forms.TextBox tbxValue;
+    }
+}
+
---
+++ Winter 2015/Demos/Demo 3/Demo 3/frmMain.cs
@@ -0,0 +1,56 @@
+﻿/************************************************
+ * Project:     Demo 3
+ * Name:        AJ Armstrong
+ * Course:      CMPE1600
+ * Date:        15 Jan 2014
+ ***********************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_3
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            UpdateTextBox();
+        }
+
+        private void tbrValue_Scroll(object sender, EventArgs e)
+        {
+            UpdateTextBox();
+        }
+
+        private void UpdateTextBox()
+        {
+            tbxValue.Text = tbrValue.Value.ToString();
+        }
+
+
+        private void tbxValue_Leave(object sender, EventArgs e)
+        {
+            int value = 0;
+            if (int.TryParse(tbxValue.Text, out value))
+            {
+                if (value > tbrValue.Maximum) value = tbrValue.Maximum;
+                if (value < tbrValue.Minimum) value = tbrValue.Minimum;
+
+                tbrValue.Value = value;
+            }
+           UpdateTextBox();
+        }
+    }
+}
