bfdd9fcd008ab3e9bcd8d58e86c9239d4aa33779
AJ Armstrong <aja@nait.ca>
2015-01-26 12:16:48 -0700

    Demo 5

---
+++ Winter 2015/Demos/Demo 5/Demo 5/frmMain.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Demo_5
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lbStooges = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.Location = new System.Drawing.Point(77, 13);
+            this.nudNumber.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudNumber.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(117, 20);
+            this.nudNumber.TabIndex = 0;
+            this.nudNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudNumber.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNumber.ValueChanged += new System.EventHandler(this.nudNumber_ValueChanged);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(12, 15);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(44, 13);
+            this.lblNumber.TabIndex = 1;
+            this.lblNumber.Text = "Number";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(61, 171);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lbStooges
+            // 
+            this.lbStooges.FormattingEnabled = true;
+            this.lbStooges.Location = new System.Drawing.Point(16, 39);
+            this.lbStooges.Name = "lbStooges";
+            this.lbStooges.Size = new System.Drawing.Size(178, 82);
+            this.lbStooges.TabIndex = 3;
+            this.lbStooges.SelectedIndexChanged += new System.EventHandler(this.lbStooges_SelectedIndexChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(206, 227);
+            this.Controls.Add(this.lbStooges);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.nudNumber);
+            this.Name = "frmMain";
+            this.Text = "Demo 4";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudNumber;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ListBox lbStooges;
+    }
+}
+
---
+++ Winter 2015/Demos/Demo 5/Demo 5/frmMain.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_5
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (lbStooges.SelectedIndex == -1) //No selection
+                MessageBox.Show("Please select your favorite stooge.");
+            else
+                MessageBox.Show("Your favorite stooge is apparently " +
+                    lbStooges.SelectedItem);
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            nudNumber.Value = 1;
+            nudNumber.Minimum = 0;
+            lbStooges.Items.Add("Curly");
+            lbStooges.Items.Add("Moe");
+            lbStooges.Items.Add("Larry");
+            lbStooges.Items.Add("Curly Joe");
+            lbStooges.Items.Add("Shemp");
+            nudNumber.Maximum = lbStooges.Items.Count - 1;
+            lbStooges.SelectedIndex = 1;
+        }
+
+        private void nudNumber_ValueChanged(object sender, EventArgs e)
+        {
+            lbStooges.SelectedIndex = (int)nudNumber.Value;
+        }
+
+        private void lbStooges_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            nudNumber.Value = lbStooges.SelectedIndex;
+        }
+    }
+}
