1fe2461d5d285d931a28d4cacaffa9df9c11376c
AJ Armstrong <aja@nait.ca>
2015-03-02 12:09:52 -0700

    Demo 8 - Delegates complete

--- Winter 2015/Demos/Demo 8/Demo 8/frmMain.Designer.cs
+++ Winter 2015/Demos/Demo 8/Demo 8/frmMain.Designer.cs
@@ -28,20 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnStartThread = new System.Windows.Forms.Button();
+            this.lblThread = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnStartThread
+            // 
+            this.btnStartThread.Location = new System.Drawing.Point(12, 12);
+            this.btnStartThread.Name = "btnStartThread";
+            this.btnStartThread.Size = new System.Drawing.Size(128, 23);
+            this.btnStartThread.TabIndex = 0;
+            this.btnStartThread.Text = "Start Thread (boom!)";
+            this.btnStartThread.UseVisualStyleBackColor = true;
+            this.btnStartThread.Click += new System.EventHandler(this.ShowMessage);
+            // 
+            // lblThread
+            // 
+            this.lblThread.AutoSize = true;
+            this.lblThread.Location = new System.Drawing.Point(147, 17);
+            this.lblThread.Name = "lblThread";
+            this.lblThread.Size = new System.Drawing.Size(97, 13);
+            this.lblThread.TabIndex = 1;
+            this.lblThread.Text = "Thread not started.";
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(354, 324);
+            this.ClientSize = new System.Drawing.Size(277, 53);
+            this.Controls.Add(this.lblThread);
+            this.Controls.Add(this.btnStartThread);
             this.Name = "frmMain";
             this.Text = "Demo 8 - Multithreading";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStartThread;
+        private System.Windows.Forms.Label lblThread;
     }
 }
 
--- Winter 2015/Demos/Demo 8/Demo 8/frmMain.cs
+++ Winter 2015/Demos/Demo 8/Demo 8/frmMain.cs
@@ -6,15 +6,106 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Demo_8
 {
     public partial class frmMain : Form
     {
+        private Thread thready = null;
+        //define a type for my simple delegate that takes void and returns void
+        delegate void delVoidVoid();
+        //Delegate that accepts an int and returns an int
+        delegate int delIntIntInt(int iOneValue, int iTwoValue);
+        //Used with our counting thread
+        delegate int delIntInt(int value);
+
         public frmMain()
         {
             InitializeComponent();
+
+            //This is just some console demo stuff.  
+            //Should run as soon as form starts
+
+            //UNICAST
+            delVoidVoid bobSaysOne = SayOne;
+            bobSaysOne();
+
+            //MULTICAST
+            delVoidVoid bobSaysOneTwo = SayOne;
+            bobSaysOneTwo += SayTwo;
+            bobSaysOneTwo();
+
+            //Parameters!
+            delIntIntInt larryScreamsNumbers = SayOnesandTwos;
+            int x = larryScreamsNumbers(3,4);
+            Console.WriteLine("Larry screamed " +  x + " numbers");
         }
+
+        void SayOne()
+        { Console.WriteLine("ONE!"); }
+
+        void SayTwo()
+        { Console.WriteLine("TWO!"); }
+
+        int SayOnesandTwos(int ones, int twos)
+        {
+            for (int i = 0; i < ones; ++i)
+                Console.WriteLine("ONE!");
+            for (int i = 0; i < twos; ++i)
+                Console.WriteLine("TWO!");
+            return ones + twos; //number of lines typed.
+        }
+
+        private void btnStartThread_Click(object sender, EventArgs e)
+        {
+            thready = new Thread(new ThreadStart(CountThread));
+            thready.IsBackground = true;
+            thready.Start();
+        }
+
+        private void CountThread()
+        {
+            for (int i = 0; i < 10; ++i)
+            {
+                Thread.Sleep(1000);
+                //Console is a shared resource, so works fine.
+                Console.WriteLine("This is your thread speaking. " +
+                    "The current count is " + i);
+
+                //This, however, throws a cross-thead exception.  No playing
+                //with form controls from another thread.
+                //lblThread.Text = "Thread is counting: " + i;
+
+                //So, let's try it with a delegate and invoke, instead
+                if(InvokeRequired) //Am I running in a thread?
+                {
+                    try
+                    {
+                        Invoke(new delIntInt(updateLabel),i);
+                    }
+                    catch (ObjectDisposedException)
+                    {
+                        System.Diagnostics.Trace.WriteLine("The thread is dead, Jim!" +
+                             " I'm a doctor, not a seamstress!");
+                    }
+                }
+            }
+
+
+        }
+
+        int updateLabel(int value)
+        {
+            lblThread.Text = "Thread: " + value;
+            return value;
+        }
+
+        //This is my custom event handler, which I multicast in frmMain.Designer.cs
+        private void ShowMessage(object sender, EventArgs e)
+            {
+                MessageBox.Show("Holy ^&%$snacks!  Multicast!!");
+            }
     }
 }
