fdd72a1c9e127aa2b08b98bbaf06358013a07145
AJ Armstrong <aja@nait.ca>
2015-03-17 09:18:17 -0600

    Demo 10

---
+++ Winter 2015/Demos/Demo 10/Demo 10/frmMain.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Demo_10
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btnStopGo = new System.Windows.Forms.Button();
+            this.msMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showMessageBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmrToggle = new System.Windows.Forms.Timer(this.components);
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.msMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(12, 37);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(444, 256);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            this.pbImage.Click += new System.EventHandler(this.pbImage_Click);
+            // 
+            // btnStopGo
+            // 
+            this.btnStopGo.Enabled = false;
+            this.btnStopGo.Location = new System.Drawing.Point(185, 308);
+            this.btnStopGo.Name = "btnStopGo";
+            this.btnStopGo.Size = new System.Drawing.Size(75, 23);
+            this.btnStopGo.TabIndex = 1;
+            this.btnStopGo.Text = "Go";
+            this.btnStopGo.UseVisualStyleBackColor = true;
+            this.btnStopGo.Click += new System.EventHandler(this.btnStopGo_Click);
+            // 
+            // msMenu
+            // 
+            this.msMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.msMenu.Location = new System.Drawing.Point(0, 0);
+            this.msMenu.Name = "msMenu";
+            this.msMenu.Size = new System.Drawing.Size(468, 24);
+            this.msMenu.TabIndex = 2;
+            this.msMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImageToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showMessageBoxToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // showMessageBoxToolStripMenuItem
+            // 
+            this.showMessageBoxToolStripMenuItem.Name = "showMessageBoxToolStripMenuItem";
+            this.showMessageBoxToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.showMessageBoxToolStripMenuItem.Text = "Show Message Box";
+            this.showMessageBoxToolStripMenuItem.Click += new System.EventHandler(this.showMessageBoxToolStripMenuItem_Click);
+            // 
+            // loadImageToolStripMenuItem
+            // 
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadImageToolStripMenuItem.Text = "Load Image";
+            this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
+            // 
+            // tmrToggle
+            // 
+            this.tmrToggle.Tick += new System.EventHandler(this.tmrToggle_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 343);
+            this.Controls.Add(this.btnStopGo);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.msMenu);
+            this.MainMenuStrip = this.msMenu;
+            this.Name = "frmMain";
+            this.Text = "Demo 10";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.msMenu.ResumeLayout(false);
+            this.msMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button btnStopGo;
+        private System.Windows.Forms.MenuStrip msMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showMessageBoxToolStripMenuItem;
+        private System.Windows.Forms.Timer tmrToggle;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+    }
+}
+
---
+++ Winter 2015/Demos/Demo 10/Demo 10/frmMain.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo_10
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void showMessageBoxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I am a message!");
+        }
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Set up file open dialog for the way I want it
+            ofdFile.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            ofdFile.Filter ="PNG Files (*.png)|*.png|JPG Files (*.jpeg)" +
+                "|*.jpeg|GIF Files (*.gif)|*.gif|All Files (*.*)|*.*";
+
+            DialogResult dr = ofdFile.ShowDialog();
+            if(dr == DialogResult.OK)
+            {
+                pbImage.Image = Image.FromFile(ofdFile.FileName);
+                btnStopGo.Enabled = true;
+            }
+        }
+
+        private void pbImage_Click(object sender, EventArgs e)
+        {
+           // MessageBox.Show("That tickles!");
+        }
+
+        private void btnStopGo_Click(object sender, EventArgs e)
+        {
+            switch (btnStopGo.Text)
+            {
+                case "Stop":  //We're stopped, time to go
+                    btnStopGo.Text = "Go";
+                    tmrToggle.Enabled = false;
+                    break;
+                case "Go": //We're going, let's stop/
+                    btnStopGo.Text = "Stop";
+                    tmrToggle.Interval = 1500;
+                    tmrToggle.Enabled = true;
+                    break;
+                default: //What the hell?
+                    tmrToggle.Enabled = false;
+                    break;
+            }
+        }
+
+        private void tmrToggle_Tick(object sender, EventArgs e)
+        {
+            pbImage.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            pbImage.Refresh();
+        }
+    }
+}
