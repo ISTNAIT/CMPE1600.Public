d378452f19ea210f4092f2c8cbc4786a02b1ffe3
AJ Armstrong <aja@nait.ca>
2015-03-23 19:22:55 -0600

    Initial layout for demo 11.

---
+++ Winter 2015/Demos/Demo 11/Demo 11/frmMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Demo_11
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetName = new System.Windows.Forms.Button();
+            this.pbPenguins = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPenguins)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetName
+            // 
+            this.btnGetName.Location = new System.Drawing.Point(109, 266);
+            this.btnGetName.Name = "btnGetName";
+            this.btnGetName.Size = new System.Drawing.Size(147, 23);
+            this.btnGetName.TabIndex = 0;
+            this.btnGetName.Text = "Name Your Penguin";
+            this.btnGetName.UseVisualStyleBackColor = true;
+            // 
+            // pbPenguins
+            // 
+            this.pbPenguins.Image = global::Demo_11.Resources.Penguins;
+            this.pbPenguins.InitialImage = null;
+            this.pbPenguins.Location = new System.Drawing.Point(12, 12);
+            this.pbPenguins.Name = "pbPenguins";
+            this.pbPenguins.Size = new System.Drawing.Size(352, 225);
+            this.pbPenguins.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPenguins.TabIndex = 1;
+            this.pbPenguins.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 240);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(0, 13);
+            this.lblName.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 301);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pbPenguins);
+            this.Controls.Add(this.btnGetName);
+            this.Name = "frmMain";
+            this.Text = "Demo 11";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPenguins)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetName;
+        private System.Windows.Forms.PictureBox pbPenguins;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
