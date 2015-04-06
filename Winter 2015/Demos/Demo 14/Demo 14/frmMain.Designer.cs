6ece0cb4d8222e4eab262e6ce8e3f745ea437b93
AJ Armstrong <aja@nait.ca>
2015-04-06 12:21:15 -0600

    ICA 15 - Threading demo

---
+++ Winter 2015/Demos/Demo 14/Demo 14/frmMain.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Demo_14
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.nudMax = new System.Windows.Forms.NumericUpDown();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lbTriples = new System.Windows.Forms.ListBox();
+            this.lblValues = new System.Windows.Forms.Label();
+            this.pbProgess = new System.Windows.Forms.ProgressBar();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tmrTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nudMax)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudMax
+            // 
+            this.nudMax.Increment = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.nudMax.Location = new System.Drawing.Point(13, 35);
+            this.nudMax.Maximum = new decimal(new int[] {
+            -294967296,
+            0,
+            0,
+            0});
+            this.nudMax.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudMax.Name = "nudMax";
+            this.nudMax.Size = new System.Drawing.Size(120, 20);
+            this.nudMax.TabIndex = 0;
+            this.nudMax.Value = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(13, 13);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblMax.Size = new System.Drawing.Size(111, 13);
+            this.lblMax.TabIndex = 1;
+            this.lblMax.Text = "Maximum Hypotenuse";
+            // 
+            // lbTriples
+            // 
+            this.lbTriples.FormattingEnabled = true;
+            this.lbTriples.Location = new System.Drawing.Point(139, 37);
+            this.lbTriples.Name = "lbTriples";
+            this.lbTriples.Size = new System.Drawing.Size(281, 160);
+            this.lbTriples.TabIndex = 2;
+            // 
+            // lblValues
+            // 
+            this.lblValues.AutoSize = true;
+            this.lblValues.Location = new System.Drawing.Point(139, 13);
+            this.lblValues.Name = "lblValues";
+            this.lblValues.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblValues.Size = new System.Drawing.Size(101, 13);
+            this.lblValues.TabIndex = 3;
+            this.lblValues.Text = "Pythagorean Triples";
+            // 
+            // pbProgess
+            // 
+            this.pbProgess.Location = new System.Drawing.Point(13, 211);
+            this.pbProgess.Name = "pbProgess";
+            this.pbProgess.Size = new System.Drawing.Size(407, 23);
+            this.pbProgess.TabIndex = 4;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 62);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(108, 135);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tmrTimer
+            // 
+            this.tmrTimer.Enabled = true;
+            this.tmrTimer.Tick += new System.EventHandler(this.tmrTimer_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 246);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pbProgess);
+            this.Controls.Add(this.lblValues);
+            this.Controls.Add(this.lbTriples);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.nudMax);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "frmMain";
+            this.Text = "Pythagorean Triples";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudMax)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudMax;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.ListBox lbTriples;
+        private System.Windows.Forms.Label lblValues;
+        private System.Windows.Forms.ProgressBar pbProgess;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer tmrTimer;
+    }
+}
+
---
+++ Winter 2015/Demos/Demo 14/Demo 14/frmMain.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Demo_14
+{
+    public struct Parameters
+    {
+        public ulong min;
+        public ulong max;
+
+        public Parameters(ulong mini, ulong maxi)
+        {
+            min = mini;
+            max = maxi;
+        }
+    }
+
+    public partial class frmMain : Form
+    {
+
+        public List<Thread> Threads = null;
+        public delegate void delUpdateProgress();
+        public delegate void delUpdateTriples(string triple);
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+        }
+
+        //Add 1% to progress par
+        private void UpdateProgress()
+        {
+            if (pbProgess.Value < 100)
+                pbProgess.Value += 1;
+            this.Refresh();
+        }
+
+        private void UpdateList(string triple)
+        {
+            lbTriples.Items.Add(triple);
+            //this.Refresh();
+        }
+
+        private void ThreadWork(object pars)
+        {
+            try
+            {
+                if (pars is Parameters)
+                {
+                    Parameters vals = (Parameters)pars;
+                    SearchForTriples(vals.min, vals.max);
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                MessageBox.Show("It's dead, Jim");
+            }
+        }
+
+        private void SearchForTriples(ulong first, ulong last)
+        {
+            ulong range = last - first;
+            ulong interval = range / 10ul;
+            //a is smallest side, b is larger size, c is hypotenuse
+            for (ulong c = first; c <= last; ++c) //Possible hypotenuses
+            {
+                //Update my progress bar
+                if (InvokeRequired)
+                {
+                    Invoke(new delUpdateProgress(UpdateProgress));
+                }
+                else
+                {
+                    if (c % interval == 0)
+                        UpdateProgress();
+                }
+                for (ulong b = 1; b <= c; ++b)
+                    for (ulong a = 1; a <= b; ++a)
+                    {
+                        if (a * a + b * b == c * c) //found a triple!
+                            if (InvokeRequired)
+                            {
+                                Invoke(new delUpdateTriples(UpdateList), a + "," + b + "," + c);
+                            }
+                            else //running in same thread
+                            {
+                                UpdateList(a + "," + b + "," + c);
+                            }
+                    }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            ulong min = 1ul;
+            ulong max = (ulong)nudMax.Value;
+            ulong interval = (max - min) / 10ul;
+            ulong current = min;
+            Thread t = null;
+            Threads = new List<Thread>(10);
+            for (int i = 0; i < 9; ++i)
+            {
+                t = new Thread(ThreadWork);
+                t.IsBackground = true;
+                t.Start(new Parameters(current, current + interval));
+                Threads.Add(t);
+                current += interval + 1;
+            }
+            t = new Thread(ThreadWork);
+            t.IsBackground = true;
+            t.Start(new Parameters(current, max));
+            Threads.Add(t);
+            SearchForTriples(85ul, (ulong)nudMax.Value);
+        }
+
+        private void tmrTimer_Tick(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            if (Threads != null)
+                foreach (Thread t in Threads)
+                    if (t.ThreadState != ThreadState.Stopped)
+                        btnStart.Enabled = false;
+        }
+    }
+}
