49f21f54259b4868a58c63c38f24cbd4bb7e98c6
AJ Armstrong <aja@nait.ca>
2015-04-13 12:18:56 -0600

    Demo of Second LE Question

---
+++ Winter 2015/Demos/LE 2/LE 2/frmMain.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace LE_2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOutput.Location = new System.Drawing.Point(13, 13);
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ReadOnly = true;
+            this.tbOutput.Size = new System.Drawing.Size(451, 20);
+            this.tbOutput.TabIndex = 0;
+            this.tbOutput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 46);
+            this.Controls.Add(this.tbOutput);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Lab Exam #2";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Winter 2015/Demos/LE 2/LE 2/frmMain.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LE_2
+{
+    public struct SRange
+    {
+        public int lower;
+        public int upper;
+        public SRange(int lower, int upper)
+        {
+            this.lower = lower;
+            this.upper = upper;
+        }
+    }
+
+    public partial class frmMain : Form
+    {
+        public List<Thread> threads = new List<Thread>(4);
+        public delegate int delIntSRange(SRange range);
+        public delegate void delvoidInt(int value);
+        public int count = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            int lower = 1000000000;
+            int upper = 1100000000;
+            int range = upper - lower;
+            int inc = range / 4;
+            int current = lower;
+            Thread t = null;
+            SRange s;
+
+            tbOutput.Text = "Calculating...";
+
+            for (int i = 0; i < 3; ++i)
+            {
+                s = new SRange(current, current + inc);
+                current = current + inc + 1;
+                t = new Thread(new ParameterizedThreadStart(ThreadWork));
+                t.Start(s);
+                t.IsBackground = true;
+                threads.Add(t);
+            }
+
+            s = new SRange(current, upper);
+            t = new Thread(new ParameterizedThreadStart(ThreadWork));
+            t.Start(s);
+            t.IsBackground = true;
+            threads.Add(t);
+
+            timer.Enabled = true;
+        }
+
+        public void UpdateOutput(int value)
+        {
+            count += value;
+            //tbOutput.Text = "4:" + count.ToString();
+        }
+
+        //returns true sum digits is same for number and sqrt
+        bool TestValue (int val)
+        {
+            return (SumDigits(val) == SumDigits((int)Math.Sqrt(val)));
+        }
+
+        int CountTrue(SRange range)
+        {
+            int total = 0;
+            for (int i = range.lower; i <= range.upper; ++i)
+                if (TestValue(i)) total++;
+            return total;
+        }
+
+        void ThreadWork(object Range)
+        {
+            int count = 0;
+            if (Range is SRange)
+            {
+                count = CountTrue((SRange)Range);
+            }
+
+            if(InvokeRequired)
+            {
+                try
+                {
+                    Invoke(new delvoidInt(UpdateOutput), count);
+
+                }
+                catch
+                {
+                    MessageBox.Show("Aiiiieeeee!!!");
+                }
+            }
+        }
+
+
+        int SumDigits(int val)
+        {
+            int sum = 0;
+            while(val > 0)
+            {
+                //Add the rightmost digit
+                sum += (val % 10);
+                //Right shift (base 10)
+                val /= 10;
+            }
+            return sum;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            bool active = false;
+            foreach (Thread t in threads)
+                if (t.ThreadState != ThreadState.Stopped)
+                    active = true;
+            if (!active)
+                tbOutput.Text = "4:" + count;
+        }
+    }
+}
